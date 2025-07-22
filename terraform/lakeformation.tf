c4ef76353d9a3affdcbaccb6b72908876ad95919
Marcos Foglino <marcos.foglino@caylent.com>
2025-07-22 16:55:38 -0300

    lakeformation permissions for glue etl role

--- terraform/lakeformation.tf
+++ terraform/lakeformation.tf
@@ -1,26 +1,28 @@
-# resource "aws_lakeformation_resource" "s3tables_data_location" {
-#   #arn = "arn:aws:s3tables:${local.region}:${local.account_id}:bucket/${aws_s3tables_table_bucket.table_bucket.name}"
-#   arn = "arn:aws:s3tables:us-east-1:131578276461:bucket/datalake-example-s3tables"
-# }
-#
-# resource "aws_lakeformation_permissions" "glue_data_location_access" {
-#   principal = aws_iam_role.glue_role.arn
-#
-#   data_location {
-#     arn = aws_lakeformation_resource.s3tables_data_location.arn
-#   }
-#
-#   permissions = ["DATA_LOCATION_ACCESS"]
-# }
-#
-# resource "aws_lakeformation_permissions" "glue_table_access" {
-#   principal = aws_iam_role.glue_role.arn
-#
-#   table {
-#     catalog_id    = local.account_id
-#     database_name = "s3tables"
-#     name          = "people"
-#   }
-#
-#   permissions = ["SELECT", "INSERT", "DELETE", "DESCRIBE", "ALTER"]
-# }
+resource "aws_glue_catalog_database" "s3tablesmarcos" {
+  name = "s3tablesmarcos"
+}
+
+resource "aws_lakeformation_permissions" "glue_database_access" {
+  principal = aws_iam_role.glue_role.arn
+
+  database {
+    name = aws_glue_catalog_database.s3tablesmarcos.name
+  }
+
+  permissions = ["DESCRIBE"]
+
+  depends_on = [aws_glue_catalog_database.s3tablesmarcos]
+}
+
+resource "aws_lakeformation_permissions" "glue_all_tables_access" {
+  principal = aws_iam_role.glue_role.arn
+
+  table {
+    database_name = aws_glue_catalog_database.s3tablesmarcos.name
+    wildcard      = true
+  }
+
+  permissions = ["SELECT", "INSERT", "DELETE", "DESCRIBE", "ALTER"]
+
+  depends_on = [aws_glue_catalog_database.s3tablesmarcos]
+}
