f67f4ef304650e5a5a930336ed1058d28c410f7b
Marcos Foglino <marcos.foglino@caylent.com>
2025-07-22 17:34:07 -0300

    lakeformation for admin

--- terraform/lakeformation.tf
+++ terraform/lakeformation.tf
@@ -2,27 +2,36 @@ resource "aws_glue_catalog_database" "s3tablesmarcos" {
   name = "s3tablesmarcos"
 }
 
-resource "aws_lakeformation_permissions" "glue_database_access" {
-  principal = aws_iam_role.glue_role.arn
+locals {
+  principals = {
+    glue_role = aws_iam_role.glue_role.arn
+    sso_user  = "arn:aws:iam::131578276461:role/aws-reserved/sso.amazonaws.com/AWSReservedSSO_AdministratorAccess_370d0a9b30d49146"
+  }
+}
+
+resource "aws_lakeformation_permissions" "database_access" {
+  for_each = local.principals
+
+  principal = each.value
 
   database {
     name = aws_glue_catalog_database.s3tablesmarcos.name
   }
 
-  permissions = ["DESCRIBE"]
-
+  permissions = ["ALL"]
   depends_on = [aws_glue_catalog_database.s3tablesmarcos]
 }
 
-resource "aws_lakeformation_permissions" "glue_all_tables_access" {
-  principal = aws_iam_role.glue_role.arn
+resource "aws_lakeformation_permissions" "all_tables_access" {
+  for_each = local.principals
+
+  principal = each.value
 
   table {
     database_name = aws_glue_catalog_database.s3tablesmarcos.name
     wildcard      = true
   }
 
-  permissions = ["SELECT", "INSERT", "DELETE", "DESCRIBE", "ALTER"]
-
+  permissions = ["ALL"]
   depends_on = [aws_glue_catalog_database.s3tablesmarcos]
 }
