aafa2fcd4e9a60eeddce9dfcadf2de16ede878ef
Marcos Foglino <marcos.foglino@caylent.com>
2025-07-24 16:56:35 -0300

    testing bedrock knowledge base

---
+++ tests/s3vectors/querying_knowledge_base.py
@@ -0,0 +1,21 @@
+import boto3
+
+def query_movies_kb():
+    bedrock_agent = boto3.client('bedrock-agent-runtime')
+
+    response = bedrock_agent.retrieve(
+        knowledgeBaseId='IE3VBIJWP7',
+        retrievalQuery={
+            'text': 'What movies were directed by Quentin Tarantino?'
+        }
+    )
+
+    for result in response['retrievalResults']:
+        #print(f"Result: {result}")
+        print(f"Metadata:{result['metadata']}")
+        print(f"Content: {result['content']['text']}")
+        print(f"Score: {result['score']}")
+        print("---")
+
+if __name__ == "__main__":
+    query_movies_kb()
---
+++ tests/s3vectors/test_querying_knowledge_database.py
@@ -0,0 +1,5 @@
+from tests.s3vectors.querying_knowledge_base import query_movies_kb
+
+
+def test_querying():
+    query_movies_kb()
